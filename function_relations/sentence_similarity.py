5dfe47e5ec53fd839b1670ee2f14b9ba26dc13fc
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-24 18:46:01 +0530

    modify sentence similarity script

--- function_relations/sentence_similarity.py
+++ function_relations/sentence_similarity.py
@@ -1,33 +1,22 @@
 #importing SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 import json
 import os
+from sklearn.metrics.pairwise import cosine_similarity
 
 model_name = 'sentence-transformers/all-mpnet-base-v2'
 model = SentenceTransformer(model_name)
 
-DATASET_LOCATION = "./data/"
-with open(os.path.join(DATASET_LOCATION, "functions_per_file(without _ ).json"), "r") as f:
-    data = json.load(f)
+DATASET_LOCATION = "../data/"
 
-sentences = []
+with open(os.path.join(DATASET_LOCATION, "functions_per_file.json"), "r") as f:
+    data = json.load(f)
+    
+function_embeds = {}
 
 for file in data:
-    sentences.append(file["fileName"][0:-3] + ", "+ file["functions"])
-
-sentence_vecs = model.encode(sentences)
-
-input_issue = "PullRequest.is_merged does not handle token expiration"
-input_vec = model.encode(input_issue)
-
-cosineSimilarity = cosine_similarity(
-    input_vec.reshape(1, -1),
-    sentence_vecs
-)
-
-data[cosineSimilarity.argmax()]
-
-
-
-
+    for function in file['functions']:
+        if not function.startsWith("_"):
+            function_embeds[file['filename'] + '/' +  function["function_name"]] = model.encode(file['filename'] + '/' +  function["function_name"], convert_to_tensor=True)
+            
+print(len(function_embeds) + " Embeddings generated")
