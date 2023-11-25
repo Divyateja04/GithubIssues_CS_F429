bc51e476f8f18a6cbeeaefa79a1a17bc63cd4c22
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-25 15:15:49 +0530

    consider all files

--- table_based/tabl.py
+++ table_based/tabl.py
@@ -8,20 +8,39 @@ model = BartForConditionalGeneration.from_pretrained("microsoft/tapex-base-finet
 with open("../data/commits.json", "r") as f:
     data = json.load(f)
     
-data = data[:20]
-    
-# Convert every list in the dictionary to a string
 for elem in data:
     elem["files"] = ", ".join(elem["files"])
     if elem["author"] is None:
         elem["author"] = "Unknown"
     
-table = pd.DataFrame.from_dict(data)
+query = "What files can be used for adding valid Repository visibility value?"
+# Have to iterate through all slices of 25
+# since the model can only handle 25 rows at a time
+# (it's a limitation of the model)
 
-# tapex accepts uncased input since it is pre-trained on the uncased corpus
-query = "What file do I have to edit to add support for issue comments?"
-encoding = tokenizer(table=table, query=query, return_tensors="pt")
+# keep track of best answers
+best_answers = {}
+for i in range(0, len(data), 25):
+    try:
+        curr_data = data[i:i+25]
+        
+        table = pd.DataFrame.from_dict(curr_data)
+        # tapex accepts uncased input since it is pre-trained on the uncased corpus
+        encoding = tokenizer(table=table, query=query, return_tensors="pt", max_new_tokens=1024)
+        outputs = model.generate(**encoding, max_length=1024, num_beams=5, early_stopping=True)
+        answer = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+       
+        # Split string based on the comma
+        answer = answer.split(",")
+        for ans in answer:
+            if ans in best_answers.keys():
+                best_answers[ans] += 1
+            else:
+                best_answers[ans] = 1
+    except:
+        pass
 
-outputs = model.generate(**encoding)
+# print top 5 answers
+print(sorted(best_answers.items(), key=lambda x: x[1], reverse=True)[:5])
+    
 
-print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
